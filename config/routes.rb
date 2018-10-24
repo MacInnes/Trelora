8138acba89c2fc9e129ad8427e059905144ef9cb
Mesky <dmeskis@gmail.com>
2018-10-24 15:23:27 -0600

    Wrote test

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
   post '/', to: 'sessions#create'
 
-  get "/address", to: 'address_search#new'
+  get "/find", to: 'address_search#new'
 
   resources :users, only: [:create]
 end
--- spec/features/user_can_find_an_address_spec.rb
+++ spec/features/user_can_find_an_address_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 feature 'user can find an address' do
-  scenario 'user visits pricing location address screen and sees an input for finding an address' do
+  scenario 'user visits pricing location address screen and searches for an address' do
 
-    
+    visit '/find'
+    fill_in :address, with: "1860_south_marion_street-Denver-CO-80210"
+    click_on "Find Location"
+    expect(current_path).to eq(address_path)
   end
 end
+
+# As an authenticated user,
+# when i visit the pricing location address screen (slide 4),
+# I see an input field titled 'Pricing Location Address',
+# and below that a 'Find Location' button.
+# As an authenticated user,
+# when i visit the pricing location address screen,
+# and fill out a VALID address and click find location,
+# app calls to trelora transaction retrieval webservice POST "api/v0/turing/properties",
+# responds with a 200 ok and takes user to address show page.
