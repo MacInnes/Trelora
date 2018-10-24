f961b309180a15fd29a434c13db9ff248daa4082
Mesky <dmeskis@gmail.com>
2018-10-24 15:15:07 -0600

    Working on test for user finding address

---
+++ spec/features/user_can_find_an_address_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+feature 'user can find an address' do
+  scenario 'user visits pricing location address screen and sees an input for finding an address' do
+
+    
+  end
+end
