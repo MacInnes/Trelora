ebdf672a30639ac672ac83a300253715891e8543
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-25 12:51:57 -0600

    fixes address show spec, adds whole user process.

--- spec/features/address_show_spec.rb
+++ spec/features/address_show_spec.rb
@@ -1,23 +1,26 @@
 require 'rails_helper'
 
 describe 'address show page' do
-  before :each do
-    @address_facade = AddressFacade.new("1860_south_marion_street-Denver-CO-80210", "this_is_a_very_simple_auth_token_string")
-    json_response = File.open("./fixtures/address_response.json")
-    stub_request(:get, "https://www.trylora.com/api/v0/turing/properties?address=1860_south_marion_street-Denver-CO-80210&HTTP_AUTH_TOKEN=this_is_a_very_simple_auth_token_string").
-    to_return(status: 200, body: json_response)
-  end
 
   it 'displays address breakdown' do
     user = User.create(name: "Maria", email: "email@email.com", HTTP_AUTH_TOKEN: "this_is_a_very_simple_auth_token_string")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    json_response = File.read("./fixtures/address_response.json")
+    stub_request(:get, "https://www.trylora.com/api/v0/turing/properties?address=1860_south_marion_street-Denver-CO-80210&HTTP_AUTH_TOKEN=this_is_a_very_simple_auth_token_string").
+    to_return(status: 200, body: json_response)
 
-    visit '/address'
+    visit '/find'
+    fill_in :address, with: "1860_south_marion_street-Denver-CO-80210"
+    click_on "Find Location"
+
+    expect(current_path).to eq(address_path)
+    save_and_open_page
+    @address_facade = AddressFacade.new("1860_south_marion_street-Denver-CO-80210", "this_is_a_very_simple_auth_token_string")
 
     expect(page).to have_content(@address_facade.client_name)
     expect(page).to have_content(@address_facade.client_email)
-    expect(page).to have_content("Opted in: Yes")
+    expect(page).to have_content("Opted in: true")
     expect(page).to have_content(@address_facade.client_phone)
     expect(page).to have_content(@address_facade.found_us)
     expect(page).to have_content(@address_facade.enthusiasm)
@@ -30,15 +33,9 @@ describe 'address show page' do
     expect(page).to have_content(@address_facade.home_junction_low)
     expect(page).to have_content(@address_facade.home_junction_high)
     expect(page).to have_content(@address_facade.adopter_type)
-    expect(page).to have_content(@address_facade.updates)
-    #expect(page).to have_content(@address_facade.retainer)
-
-
-
-#     As an authenticated user,
-# on the address show page,
-# there is a breakdown of all data collected from the response including
-# client name, email, phone number, map (coordinates), opted in, found us, estimate (pricing_estimates), enthusiasm, timing, adopter_type, updates, stage, retainer
-# from the Find Location webservice (slide 6).
+    expect(page).to have_content("New Roof in 07/15")
+    expect(page).to have_content("Basement Updated and Finished 8/2016")
+    # expect(page).to have_content(@address_facade.retainer)
+    #add coordinates
   end
 end
