c301b36cb8f757a8888ad7706b2ba5e012cc710a
Mesky <dmeskis@gmail.com>
2018-10-26 14:58:09 -0600

    Test passes for user filling out a form, does not validate data yet

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "/find", to: 'addresses#find'
   get "/address", to: 'addresses#show'
   get "/collect", to: 'forms#new'
+  post "/collect", to: 'forms#create'
 
   resources :users, only: [:create]
 end
