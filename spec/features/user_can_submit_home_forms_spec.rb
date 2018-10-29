99a29fa582022a16b454ae38f2f1d71aaf480564
Mesky <dmeskis@gmail.com>
2018-10-29 12:51:23 -0600

    Working through my user stories

--- spec/features/user_can_submit_home_forms_spec.rb
+++ spec/features/user_can_submit_home_forms_spec.rb
@@ -22,7 +22,34 @@ feature 'user can submit home form' do
     fill_in :credit_card_number, with: "347881974288396"
     fill_in :credit_card_expiration_date, with: "2020-09-15"
     click_on "Finish"
+    expect(current_path).to eq(collect_path)
+    expect(page).to have_content("Listing Consultation Complete")
+    click_on "Dismiss"
+    expect(page).to have_button('Start', disabled: true)
     # NEED TO MAKE THIS MORE COMPREHENSIVE
+  end
+  scenario 'user fills out wrong form information' do
+    user = User.create(name: "steve", email: "steven@trel.co", HTTP_AUTH_TOKEN: "this_is_a_very_simple_auth_token_string")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    # Sets session address
+    visit '/find'
+    fill_in :address, with: "1860_south_marion_street-Denver-CO-80210"
+    click_on "Find Location"
+
+    visit '/collect'
+
+    expect(page).to have_content('Begin The Listing Consultation')
+    click_on 'Start'
+    fill_in :about_the_home, with: "A home"
+    fill_in :recommended_list_price, with: "100000"
+    fill_in :update_client_enthusiasm, with: "Stoked"
+    fill_in :buyer_agent_commission, with: "500"
+    fill_in :about_the_seller, with: nil
+    fill_in :credit_card_number, with: "347881974288396"
+    fill_in :credit_card_expiration_date, with: "2020-09-15"
+    click_on "Finish"
+    expect(page).to have_content("Invalid form data.")
   end
 end
 # As an authenticated user,
