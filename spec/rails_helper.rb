b75d69927154ef1a164dad562357fd0cd95bfa28
Tara Craig <tara.craig7@gmail.com>
2018-10-31 21:28:42 -0600

    Add VCR to gemfile, VCR cassettes to address show spec.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -83,3 +83,8 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
    end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
