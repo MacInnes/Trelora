366b1a19f4a2a4557c765763a84183ebbd878cf3
MacInnes <califormula@gmail.com>
2018-11-01 16:51:10 -0600

    bug fix for invalid address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -8,8 +8,9 @@ class AddressesController < ApplicationController
   def show
     session[:address] = params[:address] unless params[:address].nil?
     @address_facade = AddressFacade.new(session[:address], current_user.HTTP_AUTH_TOKEN)
-    session[:formatted_address] = @address_facade.address
-    if @address_facade.invalid?
+    if !@address_facade.invalid?
+      session[:formatted_address] = @address_facade.address
+    else
       flash[:failure] = "Address Not Found"
       redirect_to '/find'
     end
