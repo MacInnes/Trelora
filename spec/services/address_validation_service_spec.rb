3504ca0a981994ce0792e6fe8f0367fdcc04e033
Tara Craig <tara.craig7@gmail.com>
2018-10-31 22:43:23 -0600

    Small fixes for rubocop.

--- spec/services/address_validation_service_spec.rb
+++ spec/services/address_validation_service_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 describe AddressValidationService do
   before :each do
-    @valid_avs = AddressValidationService.new(about_the_home: "Nice",
-                                        recommended_list_price: "100000000",
-                                        update_client_enthusiasm: "Excited",
-                                        buyer_agent_commission: "$500",
-                                        about_the_seller: "Cool guy",
-                                        credit_card_number: "347881974288396",
-                                        date: {credit_card_expiration_month: "10",
-                                        credit_card_expiration_year: "2018"}
-                                        )
+    @valid_avs = AddressValidationService.new(about_the_home: 'Nice',
+                                        recommended_list_price: '100000000',
+                                        update_client_enthusiasm: 'Excited',
+                                        buyer_agent_commission: '$500',
+                                        about_the_seller: 'Cool guy',
+                                        credit_card_number: '347881974288396',
+                                        date:
+                                          { credit_card_expiration_month: '10',
+                                          credit_card_expiration_year: '2018' })
     @invalid_avs = AddressValidationService.new(about_the_home: nil,
-                                        recommended_list_price: "one thousand",
+                                        recommended_list_price: 'one thousand',
                                         update_client_enthusiasm: nil,
-                                        buyer_agent_commission: "five hundred",
+                                        buyer_agent_commission: 'five hundred',
                                         about_the_seller: nil,
-                                        credit_card_number: "34788197428",
-                                        date: {credit_card_expiration_month: nil,
-                                        credit_card_expiration_year: nil}
-                                        )
+                                        credit_card_number: '34788197428',
+                                        date:
+                                          { credit_card_expiration_month: nil,
+                                          credit_card_expiration_year: nil })
   end
   describe 'validates that address information is valid' do
     describe 'valid' do
