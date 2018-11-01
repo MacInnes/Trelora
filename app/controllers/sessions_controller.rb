09ae0ef8d0163a7a2bebafdb992ad30f056eb308
Tara Craig <tara.craig7@gmail.com>
2018-11-01 13:40:43 -0600

    Refactor create method to fewer lines.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,21 @@
 class SessionsController < ApplicationController
-
-
-  def new
-
-  end
+  def new; end
 
   def create
-    # TODO: refactor this mess
     member_search = MemberSearch.new
     member = member_search.find_member(params[:email], params[:password])
     if member
       user = User.find_by(HTTP_AUTH_TOKEN: member[:user][:HTTP_AUTH_TOKEN])
       if user
         user.update(member[:user])
-        session[:id] = user.id
-        redirect_to "/find"
       else
         user = User.create(member[:user])
-        session[:id] = user.id
-        redirect_to "/find"
       end
+      session[:id] = user.id
+      redirect_to '/find'
     else
-      flash.now[:notice] = "Invalid login information."
+      flash.now[:notice] = 'Invalid login information.'
       render :new
     end
-
   end
-
 end
