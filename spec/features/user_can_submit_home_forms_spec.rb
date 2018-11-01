9413e2db97b3ca522c90b58f61ce6c092475e43e
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-01 17:39:51 -0600

    fixes spacing issues for user can submit home forms spec.

--- spec/features/user_can_submit_home_forms_spec.rb
+++ spec/features/user_can_submit_home_forms_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'user can submit home form' do
   scenario 'user fills out forms and submits them' do
-    user = User.create(name: "steve", email: "steven@trel.co", HTTP_AUTH_TOKEN: "this_is_a_very_simple_auth_token_string")
+    user = User.create(name: 'steve', email: 'steven@trel.co', HTTP_AUTH_TOKEN: 'this_is_a_very_simple_auth_token_string')
     addresses = [Address.new('1860_south_marion_street-Denver-CO-80210'), Address.new('910-portland_place-Boulder-CO-80304')]
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     allow_any_instance_of(ApplicationController).to receive(:find_addresses).and_return(addresses)
@@ -11,8 +11,8 @@ feature 'user can submit home form' do
     visit '/find'
 
     VCR.use_cassette('submit-home-passing') do
-      select "1860 South Marion Street Denver Co 80210", from: :address
-      click_on "Find Location"
+      select '1860 South Marion Street Denver Co 80210', from: :address
+      click_on 'Find Location'
     end
 
     visit collect_path
@@ -64,6 +64,6 @@ feature 'user can submit home form' do
     VCR.use_cassette('submit-home-finish-failing') do
       click_on 'Finish'
     end
-    expect(page).to have_content("Something went wrong while posting the form, please try again.")
+    expect(page).to have_content('Something went wrong while posting the form, please try again.')
   end
 end
