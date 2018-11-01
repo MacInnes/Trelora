2fdae2c2890084867485ae61ad5110d9fdf4f262
Tara Craig <tara.craig7@gmail.com>
2018-10-31 22:58:57 -0600

    Rubocop fixes.

--- spec/features/address_show_spec.rb
+++ spec/features/address_show_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe 'address show page' do
-
   it 'displays address breakdown' do
-    user = User.create(name: "Maria", email: "email@email.com", HTTP_AUTH_TOKEN: "this_is_a_very_simple_auth_token_string")
+    user = User.create(name: 'Maria',
+                       email: 'email@email.com',
+                       HTTP_AUTH_TOKEN: 'this_is_a_very_simple_auth_token_string')
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     # json_response = File.read("./fixtures/address_response.json")
@@ -11,15 +12,14 @@ describe 'address show page' do
     # to_return(status: 200, body: json_response)
     VCR.use_cassette('address-show-page') do
       visit '/find'
-      fill_in :address, with: "1860_south_marion_street-Denver-CO-80210"
-      click_on "Find Location"
+      fill_in :address, with: '1860_south_marion_street-Denver-CO-80210'
+      click_on 'Find Location'
     end
 
     expect(current_path).to eq(address_path)
 
     VCR.use_cassette('address-show-page') do
-      address_facade = AddressFacade.new("1860_south_marion_street-Denver-CO-80210", "this_is_a_very_simple_auth_token_string")
-
+      address_facade = AddressFacade.new('1860_south_marion_street-Denver-CO-80210', 'this_is_a_very_simple_auth_token_string')
 
       expect(page).to have_content(address_facade.client_name)
       expect(page).to have_content(address_facade.client_email)
@@ -29,12 +29,12 @@ describe 'address show page' do
       expect(page).to have_content(address_facade.enthusiasm)
       expect(page).to have_content(address_facade.timing)
       expect(page).to have_content(address_facade.stage)
-      expect(page).to have_content("$704,000")
-      expect(page).to have_content("$696,000")
-      expect(page).to have_content("$754,000")
-      expect(page).to have_content("$689,000")
-      expect(page).to have_content("$578,000")
-      expect(page).to have_content("$679,000")
+      expect(page).to have_content('$704,000')
+      expect(page).to have_content('$696,000')
+      expect(page).to have_content('$754,000')
+      expect(page).to have_content('$689,000')
+      expect(page).to have_content('$578,000')
+      expect(page).to have_content('$679,000')
       expect(page).to have_content(address_facade.adopter_type)
       expect(page).to have_content(address_facade.exterior_updates)
       expect(page).to have_content(address_facade.interior_updates)
