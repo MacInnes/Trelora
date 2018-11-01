cede1419093adb8e1cdd3af0579bcf3eee92f65c
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-01 17:54:34 -0600

    rubocop

--- spec/features/user_can_submit_home_forms_spec.rb
+++ spec/features/user_can_submit_home_forms_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 feature 'user can submit home form' do
   scenario 'user fills out forms and submits them' do
-    user = User.create(name: 'steve', email: 'steven@trel.co', HTTP_AUTH_TOKEN: 'this_is_a_very_simple_auth_token_string')
-    addresses = [Address.new('1860_south_marion_street-Denver-CO-80210'), Address.new('910-portland_place-Boulder-CO-80304')]
+    user = User.create(name: 'steve', email: 'steven@trel.co',
+                       HTTP_AUTH_TOKEN: 'this_is_a_very_simple_auth_token_string')
+    addresses = [Address.new('1860_south_marion_street-Denver-CO-80210'),
+                 Address.new('910-portland_place-Boulder-CO-80304')]
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     allow_any_instance_of(ApplicationController).to receive(:find_addresses).and_return(addresses)
 
